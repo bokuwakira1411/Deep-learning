e2bcc022d41fb0bf2db04447830c2e5f17a9eecb
bokuwakira1411 <158143866+bokuwakira1411@users.noreply.github.com>
2025-02-24 20:43:11 +0700

    Add files via upload

--- MLP_setup.py
+++ MLP_setup.py
@@ -5,8 +5,9 @@ import torchvision
 import torchvision.transforms as transforms
 
 class MLP(torch.nn.Module):
-    def __init__(self,input_shape, batch_size, epoch, n_hidden_nodes, n_hidden_layers, activation,drop_out=0, learning_rate = 0):
+    def __init__(self,input_shape, batch_size, epoch, n_hidden_nodes, n_hidden_layers, activation,drop_out=0, learning_rate = 0, patience =10):
         super(MLP, self).__init__()
+        self.patience= patience
         self.input_shape = input_shape
         self.batch_size = batch_size
         self.epoch = epoch
@@ -15,6 +16,7 @@ class MLP(torch.nn.Module):
         self.activation = activation
         if learning_rate == 0:
             learning_rate = 0.005
+        self.learning_rate = learning_rate
         # set up layers n_hidden_nodes, layers, activation, keep_rate, drop, out
         self.hidden_layers = torch.nn.ModuleList()
         self.hidden_layer_drop = torch.nn.ModuleList()
@@ -57,20 +59,23 @@ def train(epoch, model, train_loader,loss_function, optimizer):
         print('Train Epoch: {}\tLoss: {:.6f} Accuracy: {:.3f}%'.format(epoch, average_loss, accuracy))
 
 
-def validate(model, validation_loader):
+def validate(model, loss_function, validation_loader):
         model.eval()
         val_loss, correct = 0, 0
         for data, target in validation_loader:
             output = model(data)
-            val_loss += torch.nn.functional.nll_loss(output, target).item()
+            val_loss += loss_function(output, target).item()
             pred = output.argmax(dim=1)
             correct += pred.eq(target).sum().item()
         val_loss /= len(validation_loader)
         accuracy = 100.*correct/len(validation_loader.dataset)
         print('\nValidation set: Average loss: {:.4f}, Accuracy (%): {}/{} ({:.3f}%)\n'.format(
             val_loss, correct, len(validation_loader.dataset), accuracy))
+        return val_loss, accuracy
 def main():
     batch_size = 4
+    best_val_loss = float('inf')
+    patience_counter= 0
     transform = transforms.Compose(
         [transforms.ToTensor(), transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))]
     )
@@ -81,10 +86,18 @@ def main():
                                            download=True, transform=transform)
     validation_loader = torch.utils.data.DataLoader(testset, batch_size=4,
                                                     shuffle=False, num_workers=0, pin_memory=False)
-    model = MLP([10000, 32, 32, 3],batch_size,20, [64, 32, 16], 3, 'relu', 0.25,[.0005, 0.0005, 0.005, 0.05, 0.1])
+    model = MLP([10000, 32, 32, 3],batch_size,50, [512, 256, 128], 3, 'relu', 0.3,0.001)
     for e in range(1, model.epoch+1):
         loss_function = torch.nn.CrossEntropyLoss()
-        optimizer = torch.optim.SGD(model.parameters(), lr=0.0001)
+        optimizer = torch.optim.SGD(model.parameters(), lr=model.learning_rate)
         train(e, model, train_loader, loss_function, optimizer)
-        validate(model, validation_loader)
+        val_loss, val_acc = validate(model, loss_function, validation_loader)
+        if val_loss < best_val_loss:
+            best_val_loss = val_loss
+            patience_counter = 0
+        else:
+            patience_counter += 1
+            if patience_counter >= model.patience:
+                print("Early stopping triggered")
+                break
 main()
